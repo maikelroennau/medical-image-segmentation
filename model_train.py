d494b921b3e7107684616fcd4399377b24299f96
maikelronnau <maikel.ronnau@icloud.com>
2021-04-23 15:44:00 -0300

    Update model train script

--- model_train.py
+++ model_train.py
@@ -23,7 +23,7 @@ os.environ["TF_FORCE_GPU_ALLOW_GROWTH"] = "true"
 model_name = "AgNOR-Nucleus"
 seed = 2149
 
-epochs = 15
+epochs = 20
 batch_size = 1
 steps_per_epoch = 200
 effective_batches = steps_per_epoch * epochs
@@ -33,7 +33,7 @@ height = 960 # 240 480 960 1920
 width = 1280 # 320 640 1280 2560
 input_shape = (height, width, 3)
 
-learning_rate = 1e-5
+learning_rate = 1e-6
 
 ########
 ########
@@ -52,54 +52,56 @@ def load_images_and_masks(path, batch_size=16, target_size=(1920, 2560), seed=21
         assert image.stem.lower() == mask.stem.lower(), f"Image and mask do not correspond: {image.name} <==> {image.name}"
 
     height, width = target_size
-    images = np.zeros((len(images_paths), height, width, 3))
-    masks = np.zeros((len(masks_paths), height, width, 1))
+    images = np.zeros((len(images_paths), height, width, 3), dtype="float32")
+    masks = np.zeros((len(masks_paths), height, width, 1), dtype="float32")
 
     for i, (image, mask) in enumerate(zip(images_paths, masks_paths)):
         images[i, :, :, :] = keras.preprocessing.image.load_img(image, target_size=target_size)
         masks[i, :, :, 0] = keras.preprocessing.image.load_img(mask, target_size=target_size, color_mode="grayscale")
 
+    print(f"Loaded from '{path}'")
+    print(f"  - Images: {len(images)}")
+    print(f"  - Masks: {len(masks)}")
+
     if augment:
         datagen_arguments = dict(
-            featurewise_center=True,
-            featurewise_std_normalization=True,
-            rotation_range=50,
-            width_shift_range=0.05,
-            height_shift_range=0.05,
-            shear_range=0.1,
-            zoom_range=0.5,
-            fill_mode="reflect",
-            horizontal_flip=True,
-            vertical_flip=True,
-            rescale=1./255.
+            # featurewise_center=True,
+            # featurewise_std_normalization=True,
+            # rotation_range=10,
+            # width_shift_range=0.05,
+            # height_shift_range=0.05,
+            # shear_range=0.05,
+            # zoom_range=0.05,
+            # fill_mode="reflect",
+            # horizontal_flip=True,
+            # vertical_flip=True,
+            # rescale=1./255.
         )
 
         images_datagen = keras.preprocessing.image.ImageDataGenerator(**datagen_arguments)
-        images_datagen.fit(images)
+        # images_datagen.fit(images)
 
-        datagen_arguments.pop("featurewise_center")
-        datagen_arguments.pop("featurewise_std_normalization")
+        # datagen_arguments.pop("featurewise_center")
+        # datagen_arguments.pop("featurewise_std_normalization")
         masks_datagen = keras.preprocessing.image.ImageDataGenerator(**datagen_arguments)
 
         if save_to_dir:
             Path(save_to_dir).mkdir(exist_ok=True)
-        train_images = images_datagen.flow(images, batch_size=batch_size, seed=seed, shuffle=True, save_prefix=save_prefix, save_to_dir=save_to_dir)
-        train_masks = masks_datagen.flow(masks, batch_size=batch_size, seed=seed, shuffle=True, save_prefix=save_prefix, save_to_dir=save_to_dir)
+        train_images = images_datagen.flow(images, batch_size=batch_size, seed=seed, shuffle=True, save_prefix=f"{save_prefix}_image", save_to_dir=save_to_dir)
+        train_masks = masks_datagen.flow(masks, batch_size=batch_size, seed=seed, shuffle=True, save_prefix=f"{save_prefix}_mask", save_to_dir=save_to_dir)
 
         return train_images, train_masks
     else:
-        train_images = images * (1./255.)
-        train_masks = masks * (1./255.)
-        return train_images, train_masks
+        return images, masks
 
 ########
 ########
 
-train_images, train_masks = load_images_and_masks("dataset/train/", target_size=(height, width), augment=True, batch_size=batch_size)
+train_images, train_masks = load_images_and_masks("dataset/train/", target_size=(height, width), augment=True, batch_size=batch_size, save_to_dir=None)
 validation_images, validation_masks = load_images_and_masks("dataset/validation/", target_size=(height, width))
 
-# for i, batch in enumerate(train_images):
-#     print(batch[0].shape, batch[1].shape)
+# for i, (images, masks) in enumerate(zip(train_images, train_masks)):
+#     print(images.shape)
 #     if i + 1 == steps_per_epoch:
 #         break
 # assert 1 == 2
@@ -126,6 +128,9 @@ def dice_coef_loss(y_true, y_pred):
 
 def make_model(input_shape, name="AgNOR"):
     inputs = keras.Input(shape=input_shape)
+
+    # normalized = keras.layers.experimental.preprocessing.Normalization()
+    rescaled = keras.layers.experimental.preprocessing.Rescaling(1./255.)
 
     conv1 = Conv2D(32, (3, 3), activation="relu", padding="same")(inputs)
     conv1 = Conv2D(32, (3, 3), activation="relu", padding="same")(conv1)
@@ -182,6 +187,7 @@ os.makedirs(checkpoint_directory)
 callbacks = [
     keras.callbacks.ReduceLROnPlateau(monitor="loss", factor=0.5, patience=10, verbose=1,  mode="auto", cooldown=1),
     keras.callbacks.ModelCheckpoint(os.path.join(checkpoint_directory, "epoch_{epoch}.h5"), monitor="val_dice_coef", save_best_only=False),
+    # keras.callbacks.TensorBoard(log_dir=os.path.join(checkpoint_directory, "logs"), histogram_freq=1, update_freq="batch")
 ]
 
 ########
