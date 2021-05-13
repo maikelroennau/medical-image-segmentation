73da930682e216ae238a352b486bb60ab07acd9e
maikelronnau <maikel.ronnau@icloud.com>
2021-05-13 12:06:26 -0300

    Add data generator as the data pipeline solution

--- evaluate.py
+++ evaluate.py
@@ -8,52 +8,37 @@ from tensorflow import keras
 from tensorflow.keras.optimizers import Adam
 
 
-def load_files(image_path, target_shape=(1920, 2560)):
-    image = tf.io.read_file(image_path)
-    image = tf.image.decode_jpeg(image, channels=3)
-    # image = tf.image.decode_image(image, channels=3)
-    image = tf.image.convert_image_dtype(image, tf.uint8)
-    image = tf.image.resize(image, target_shape)
+def data_loader(path, batch_size=32, target_shape=(1920, 2560), seed=1145):
+    image_datagen = tf.keras.preprocessing.image.ImageDataGenerator()
+    mask_datagen = tf.keras.preprocessing.image.ImageDataGenerator()
 
-    mask_path = tf.strings.regex_replace(image_path, "images", "masks")
-    supported_types = [".tif", ".tiff", ".png", ".jpg", ".jpeg"]
-    for supported_type in supported_types:
-        mask_path = tf.strings.regex_replace(mask_path, supported_type, ".png")
+    images = image_datagen.flow_from_directory(
+        directory=Path(path),
+        target_size=target_shape,
+        classes=["images"],
+        class_mode=None,
+        color_mode="rgb",
+        batch_size=batch_size,
+        seed=seed
+    )
 
-    mask = tf.io.read_file(mask_path)
-    mask = tf.image.decode_png(mask, channels=1)
-    mask = tf.image.convert_image_dtype(mask, tf.uint8)
-    mask = tf.image.resize(mask, target_shape)
-    return image, mask
+    masks = mask_datagen.flow_from_directory(
+        directory=Path(path),
+        target_size=target_shape,
+        classes=["masks"],
+        class_mode=None,
+        color_mode="grayscale",
+        batch_size=batch_size,
+        seed=seed
+    )
+
+    return images, masks
 
 
-def load_dataset(path, batch_size=32, target_shape=(1920, 2560), seed=1145):
-    images_path = Path(path).joinpath("images")
-    masks_path = Path(path).joinpath("masks")
-
-    supported_types = [".tif", ".tiff", ".png", ".jpg", ".jpeg"]
-    images_paths = [image_path for image_path in images_path.glob("*.*") if image_path.suffix.lower() in supported_types and not image_path.stem.endswith("_prediction")]
-    masks_paths = [mask_path for mask_path in masks_path.glob("*.*") if mask_path.suffix.lower() in supported_types and not mask_path.stem.endswith("_prediction")]
-
-    images_paths.sort()
-    masks_paths.sort()
-
-    assert len(images_paths) == len(masks_paths), f"Different quantity of images ({len(images_paths)}) and masks ({len(masks_paths)})"
-
-    for image_path, mask_path in zip(images_paths, masks_paths):
-        assert image_path.stem.lower() == mask_path.stem.lower(), f"Image and mask do not correspond: {image_path.name} <==> {mask_path.name}"
-
-    print(f"Dataset '{str(images_path.parent)}' contains {len(images_paths)} images and masks.")
-
-    images_paths = [str(image_path) for image_path in images_paths]
-    dataset_files = tf.data.Dataset.from_tensor_slices(images_paths)
-    dataset = dataset_files.map(lambda x: load_files(x, target_shape))
-
-    dataset = dataset.shuffle(buffer_size=len(images_paths), seed=seed)
-    # dataset = dataset.repeat()
-    dataset = dataset.batch(batch_size)
-    dataset = dataset.prefetch(buffer_size=batch_size)
-    return dataset
+def data_generator(path, batch_size=32, target_shape=(1920, 2560), seed=1145):
+    images, masks = data_loader(path, batch_size, target_shape, seed)
+    for images, masks in zip(images, masks):
+        yield (images, masks)
 
 
 def dice_coef(y_true, y_pred, smooth=1.):
@@ -78,8 +63,8 @@ def main(model, images_path="dataset/test/", test_all=False):
         input_shape = loaded_model.input_shape[1:]
         height, width, channels = input_shape
 
-        evaluate_dataset = load_dataset(images_path, batch_size=1, target_shape=(height, width))
-        loss, dice = loaded_model.evaluate(evaluate_dataset)
+        evaluate_dataset = data_generator(images_path, batch_size=1, target_shape=(height, width))
+        loss, dice = loaded_model.evaluate(evaluate_dataset, steps=10)
         print(f"Model {Path(model).name}")
         print("  - Loss: %.4f" % loss)
         print("  - Dice: %.4f" % dice)
@@ -91,13 +76,13 @@ def main(model, images_path="dataset/test/", test_all=False):
         input_shape = loaded_model.input_shape[1:]
         height, width, channels = input_shape
 
-        evaluate_dataset = load_dataset(images_path, batch_size=1, target_shape=(height, width))
+        evaluate_dataset = data_generator(images_path, batch_size=1, target_shape=(height, width))
         best = {}
 
         for i, model_path in enumerate(models):
             loaded_model = keras.models.load_model(str(model_path), custom_objects={"dice_coef_loss": dice_coef_loss, "dice_coef": dice_coef})
             loaded_model.compile(optimizer=Adam(lr=1e-5), loss=dice_coef_loss, metrics=[dice_coef])
-            loss, dice = loaded_model.evaluate(evaluate_dataset)
+            loss, dice = loaded_model.evaluate(evaluate_dataset, steps=10)
             print(f"({i+1}/{len(models)}) Model {model_path.name}")
             print("  - Loss: %.4f" % loss)
             print("  - Dice: %.4f" % dice)
--- model_train.py
+++ model_train.py
@@ -27,8 +27,10 @@ np.random.seed(seed)
 
 model_name = "AgNOR-NOR"
 
-epochs = 20
+epochs = 100
 batch_size = 1
+steps_per_epoch = 600
+validation_steps = 10
 
 height = 960 # 240 480 960 1920
 width = 1280 # 320 640 1280 2560
@@ -43,90 +45,43 @@ test_dataset_path = "dataset/nucleus/test/"
 ########
 ########
 
-def write_dataset(dataset, output_path="dataset_visualization", max_batches=None, same_dir=False):
-    output = Path(output_path)
-    images_path = output.joinpath("images")
+def data_loader(path, batch_size=32, target_shape=(1920, 2560), seed=1145):
+    image_datagen = tf.keras.preprocessing.image.ImageDataGenerator()
+    mask_datagen = tf.keras.preprocessing.image.ImageDataGenerator()
 
-    if same_dir:
-        masks_path = output.joinpath("images")
-    else:
-        masks_path = output.joinpath("masks")
+    images = image_datagen.flow_from_directory(
+        directory=Path(path),
+        target_size=target_shape,
+        classes=["images"],
+        class_mode=None,
+        color_mode="rgb",
+        batch_size=batch_size,
+        seed=seed
+    )
 
-    images_path.mkdir(exist_ok=True, parents=True)
-    masks_path.mkdir(exist_ok=True, parents=True)
+    masks = mask_datagen.flow_from_directory(
+        directory=Path(path),
+        target_size=target_shape,
+        classes=["masks"],
+        class_mode=None,
+        color_mode="grayscale",
+        batch_size=batch_size,
+        seed=seed
+    )
 
-    if max_batches:
-        if max_batches > len(dataset):
-            batches = len(dataset)
-        else:
-            batches = max_batches
-    else:
-        batches = len(dataset)
-
-    for i, batch in tqdm(enumerate(dataset), total=batches):
-        for j, (image, mask) in enumerate(zip(batch[0], batch[1])):
-            image_name = str(images_path.joinpath(f"batch_{i}_{j}.jpg"))
-            mask_name = str(masks_path.joinpath(f"batch_{i}_{j}.png"))
-            keras.preprocessing.image.save_img(image_name, image)
-            keras.preprocessing.image.save_img(mask_name, mask)
-
-        keras.backend.clear_session()
-        if i + 1 == batches:
-            break
+    return images, masks
 
 
-def load_files(image_path, target_shape=(1920, 2560)):
-    image = tf.io.read_file(image_path)
-    image = tf.image.decode_jpeg(image, channels=3)
-    # image = tf.image.decode_image(image, channels=3)
-    image = tf.image.convert_image_dtype(image, tf.uint8)
-    image = tf.image.resize(image, target_shape)
-
-    mask_path = tf.strings.regex_replace(image_path, "images", "masks")
-    supported_types = [".tif", ".tiff", ".png", ".jpg", ".jpeg"]
-    for supported_type in supported_types:
-        mask_path = tf.strings.regex_replace(mask_path, supported_type, ".png")
-
-    mask = tf.io.read_file(mask_path)
-    mask = tf.image.decode_png(mask, channels=1)
-    mask = tf.image.convert_image_dtype(mask, tf.uint8)
-    mask = tf.image.resize(mask, target_shape)
-    return image, mask
-
-
-def load_dataset(path, batch_size=32, target_shape=(1920, 2560), seed=1145):
-    images_path = Path(path).joinpath("images")
-    masks_path = Path(path).joinpath("masks")
-
-    supported_types = [".tif", ".tiff", ".png", ".jpg", ".jpeg"]
-    images_paths = [image_path for image_path in images_path.glob("*.*") if image_path.suffix.lower() in supported_types and not image_path.stem.endswith("_prediction")]
-    masks_paths = [mask_path for mask_path in masks_path.glob("*.*") if mask_path.suffix.lower() in supported_types and not mask_path.stem.endswith("_prediction")]
-
-    images_paths.sort()
-    masks_paths.sort()
-
-    assert len(images_paths) == len(masks_paths), f"Different quantity of images ({len(images_paths)}) and masks ({len(masks_paths)})"
-
-    for image_path, mask_path in zip(images_paths, masks_paths):
-        assert image_path.stem.lower() == mask_path.stem.lower(), f"Image and mask do not correspond: {image_path.name} <==> {mask_path.name}"
-
-    print(f"Dataset '{str(images_path.parent)}' contains {len(images_paths)} images and masks.")
-
-    images_paths = [str(image_path) for image_path in images_paths]
-    dataset_files = tf.data.Dataset.from_tensor_slices(images_paths)
-    dataset = dataset_files.map(lambda x: load_files(x, target_shape))
-
-    dataset = dataset.shuffle(buffer_size=len(images_paths), seed=seed)
-    # dataset = dataset.repeat()
-    dataset = dataset.batch(batch_size)
-    dataset = dataset.prefetch(buffer_size=batch_size)
-    return dataset
+def data_generator(path, batch_size=32, target_shape=(1920, 2560), seed=1145):
+    images, masks = data_loader(path, batch_size, target_shape, seed)
+    for images, masks in zip(images, masks):
+        yield (images, masks)
 
 ########
 ########
 
-train_dataset = load_dataset(train_dataset_path, batch_size=batch_size, target_shape=(height, width))
-validation_dataset = load_dataset(validation_dataset_path, batch_size=batch_size, target_shape=(height, width))
+train_dataset = data_generator(train_dataset_path, batch_size=batch_size, target_shape=(height, width))
+validation_dataset = data_generator(validation_dataset_path, batch_size=batch_size, target_shape=(height, width))
 
 ########
 ########
@@ -146,7 +101,7 @@ data_augmentation = keras.Sequential(
     [
         # layers.experimental.preprocessing.RandomRotation(0.2, seed=seed),
         layers.experimental.preprocessing.RandomFlip(mode="horizontal_and_vertical", seed=seed),
-        layers.experimental.preprocessing.RandomContrast(0.1, seed=seed),
+        # layers.experimental.preprocessing.RandomContrast(0.1, seed=seed),
         # layers.experimental.preprocessing.Rescaling(1./255.)
 
     ]
@@ -249,8 +204,10 @@ print(f"  - Checkpoints saved at: {checkpoint_directory}\n")
 keras.backend.clear_session()
 history = model.fit(
     train_dataset,
+    steps_per_epoch=steps_per_epoch,
     epochs=epochs,
     validation_data=validation_dataset,
+    validation_steps=validation_steps,
     callbacks=callbacks)
 
 end = time.time()
@@ -277,7 +234,7 @@ with open(os.path.join(checkpoint_directory, "train_config.json"), "w") as confi
 ########
 
 print("\nModel evaluation")
-test_dataset = load_dataset(test_dataset_path, batch_size=batch_size, target_shape=(height, width))
+test_dataset = data_generator(test_dataset_path, batch_size=batch_size, target_shape=(height, width))
 loss, dice = model.evaluate(test_dataset)
 print("Loss: %.4f" % loss)
 print("Dice: %.4f" % dice)
