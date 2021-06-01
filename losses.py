0ba57112f0ddfd65eff733310494a8fee40c98aa
maikelronnau <maikel.ronnau@icloud.com>
2021-06-01 20:52:51 -0300

    Update loss functions

--- losses.py
+++ losses.py
@@ -1,21 +1,21 @@
 import tensorflow as tf
 
 
-def dice_coef(y_true, y_pred, smooth=1):
-    intersection = tf.keras.backend.sum(y_true * y_pred, axis=[1, 2, 3])
-    union = tf.keras.backend.sum(y_true, axis=[1, 2, 3]) + tf.keras.backend.sum(y_pred, axis=[1, 2, 3])
-    return tf.keras.backend.mean((2 * intersection + smooth) / (union + smooth), axis=0)
+def dice_coef(y_true, y_pred, smooth=1e-7):
+    intersection = tf.keras.backend.sum(y_true * y_pred)
+    union = tf.keras.backend.sum(y_true) + tf.keras.backend.sum(y_pred)
+    return tf.keras.backend.mean((2. * intersection + smooth) / (union + smooth))
 
 
 def dice_coef_loss(y_true, y_pred):
-    return 1 - dice_coef(y_true, y_pred)
+    return 1. - dice_coef(y_true, y_pred)
 
 
-def jaccard_index(y_true, y_pred, smooth=1):
-    intersection = tf.keras.backend.abs(tf.keras.backend.sum(y_true * y_pred, axis=[1, 2, 3]))
-    union = tf.keras.backend.abs(tf.keras.backend.sum(y_true, axis=[1, 2, 3]) + tf.keras.backend.sum(y_pred, axis=[1, 2, 3]))
+def jaccard_index(y_true, y_pred, smooth=1e-7):
+    intersection = tf.keras.backend.sum(y_true * y_pred)
+    union = tf.keras.backend.sum(y_true) + tf.keras.backend.sum(y_pred)
     return (intersection + smooth) / ((union + smooth) - (intersection + smooth))
 
 
 def jaccard_index_loss(y_true, y_pred):
-    return 1 - jaccard_index(y_true, y_pred)
+    return 1. - jaccard_index(y_true, y_pred)
