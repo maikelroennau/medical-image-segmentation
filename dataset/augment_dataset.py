c27e7f6e91885913413b8ff7444107801667c8af
Maikel Maciel Rönnau <maikel.ronnau@icloud.com>
2021-07-13 00:01:37 -0300

    Reduce execution time of image augmentation'

--- dataset/augment_dataset.py
+++ dataset/augment_dataset.py
@@ -1,10 +1,39 @@
 import argparse
+import multiprocessing
 from pathlib import Path
 
 import albumentations as A
 import cv2
 import numpy as np
 from tqdm import tqdm
+
+
+def list_files(path, validate_masks=False):
+    supported_types = [".tif", ".tiff", ".png", ".jpg", ".jpeg"]
+
+    images_path = Path(path).joinpath("images")
+    masks_path = Path(path).joinpath("masks")
+
+    images_paths = [image_path for image_path in images_path.glob("*.*") if image_path.suffix.lower() in supported_types and not image_path.stem.endswith("_prediction")]
+    masks_paths = [mask_path for mask_path in masks_path.glob("*.*") if mask_path.suffix.lower() in supported_types and not mask_path.stem.endswith("_prediction")]
+
+    assert len(images_paths) > 0, f"No images found at '{images_path}'."
+    assert len(masks_paths) > 0, f"No masks found at '{masks_paths}'."
+
+    images_paths.sort()
+    masks_paths.sort()
+
+    if validate_masks:
+        assert len(images_paths) == len(masks_paths), f"Different quantity of images ({len(images_paths)}) and masks ({len(masks_paths)})"
+
+        for image_path, mask_path in zip(images_paths, masks_paths):
+            assert image_path.stem.lower().replace("image", "") == mask_path.stem.lower().replace("mask", ""), f"Image and mask do not correspond: {image_path.name} <==> {mask_path.name}"
+
+    print(f"Dataset '{str(images_path.parent)}' contains {len(images_paths)} images and masks.")
+
+    images_paths = [str(image_path) for image_path in images_paths]
+    masks_paths = [str(masks_path) for masks_path in masks_paths]
+    return images_paths, masks_paths
 
 
 def get_transformations():
@@ -16,14 +45,14 @@ def get_transformations():
     transformations.append(
         A.Compose([
             A.HorizontalFlip(p=1),
-            A.GridDistortion(p=1)
+            A.ElasticTransform(p=1)
         ])
     )
 
     transformations.append(
         A.Compose([
             A.VerticalFlip(p=1),
-            A.GridDistortion(p=1)
+            A.ElasticTransform(p=1)
         ])
     )
 
@@ -31,17 +60,17 @@ def get_transformations():
         A.Compose([
             A.HorizontalFlip(p=1),
             A.VerticalFlip(p=1),
-            A.GridDistortion(p=1)
+            A.ElasticTransform(p=1)
         ])
     )
 
     transformations.append(
         A.Compose([
-            A.GridDistortion(p=1),
+            A.ElasticTransform(p=1),
             A.HorizontalFlip(p=1),
-            A.GridDistortion(p=1),
+            A.ElasticTransform(p=1),
             A.VerticalFlip(p=1),
-            A.GridDistortion(p=1)
+            A.ElasticTransform(p=1)
         ])
     )
 
@@ -50,60 +79,58 @@ def get_transformations():
     ###########
     transformations.append(
         A.Compose([
-            A.GridDistortion(p=1),
-            A.GridDistortion(p=1)
+            A.ElasticTransform(p=1),
+            A.ElasticTransform(p=1)
         ])
     )
 
     return transformations
 
 
-def augment_dataset(input_dir, output_dir, suffix="", seed=None):
-    if seed:
-        np.random.seed(seed)
+class ImageAugmentation:
+    def __init__(self, transformation_pipeline, output_dir, suffix) -> None:
+        self.transformations = transformation_pipeline
+        self.output_dir = output_dir
+        self.suffix = suffix
 
-    supported_types = [".tif", ".tiff", ".png", ".jpg", ".jpeg"]
+        output_dir = Path(output_dir)
+        output_dir.mkdir(exist_ok=True, parents=True)
 
-    images_path = Path(input_dir).joinpath("images")
-    masks_path = Path(input_dir).joinpath("masks")
+        self.images_output = output_dir.joinpath("images")
+        self.images_output.mkdir(exist_ok=True, parents=True)
 
-    images_paths = [image_path for image_path in images_path.glob("*.*") if image_path.suffix.lower() in supported_types and not image_path.stem.endswith("_prediction")]
-    masks_paths = [mask_path for mask_path in masks_path.glob("*.*") if mask_path.suffix.lower() in supported_types and not mask_path.stem.endswith("_prediction")]
+        self.masks_output = output_dir.joinpath("masks")
+        self.masks_output.mkdir(exist_ok=True, parents=True)
 
-    images_paths.sort()
-    masks_paths.sort()
 
-    assert len(images_paths) == len(masks_paths), f"Different quantity of images ({len(images_paths)}) and masks ({len(masks_paths)})"
-
-    for image_path, mask_path in zip(images_paths, masks_paths):
-        assert image_path.stem.lower() == mask_path.stem.lower(), f"Image and mask do not correspond: {image_path.name} <==> {mask_path.name}"
-
-    print(f"Dataset '{str(images_path.parent)}' contains {len(images_paths)} images and masks.")
-
-    transformations = get_transformations()
-    output_dir = Path(output_dir)
-    output_dir.mkdir(exist_ok=True, parents=True)
-
-    images_output = output_dir.joinpath("images")
-    images_output.mkdir(exist_ok=True, parents=True)
-
-    masks_output = output_dir.joinpath("masks")
-    masks_output.mkdir(exist_ok=True, parents=True)
-
-    for i, (image_path, mask_path) in tqdm(enumerate(zip(images_paths, masks_paths)), total=len(images_paths)):
+    def __call__(self, image_path, mask_path):
+        image_path = Path(image_path)
+        mask_path = Path(mask_path)
         image = cv2.imread(str(image_path), cv2.IMREAD_COLOR)
         mask = cv2.imread(str(mask_path), cv2.IMREAD_GRAYSCALE)
 
-        cv2.imwrite(str(images_output.joinpath(f"{image_path.name}")), image)
-        cv2.imwrite(str(masks_output.joinpath(f"{mask_path.name}")), mask)
+        cv2.imwrite(str(self.images_output.joinpath(f"{image_path.name}")), image)
+        cv2.imwrite(str(self.masks_output.joinpath(f"{mask_path.name}")), mask)
 
-        for j, tranformation in enumerate(transformations):
+        for j, tranformation in enumerate(self.transformations):
             transormed = tranformation(image=image, mask=mask)
             transformed_image = transormed["image"]
             transformed_mask = transormed["mask"]
 
-            cv2.imwrite(str(images_output.joinpath(f"{image_path.stem}_t{j}{suffix}{image_path.suffix}")), transformed_image)
-            cv2.imwrite(str(masks_output.joinpath(f"{mask_path.stem}_t{j}{suffix}{mask_path.suffix}")), transformed_mask)
+            cv2.imwrite(str(self.images_output.joinpath(f"{image_path.stem}_t{j}{self.suffix}{image_path.suffix}")), transformed_image)
+            cv2.imwrite(str(self.masks_output.joinpath(f"{mask_path.stem}_t{j}{self.suffix}{mask_path.suffix}")), transformed_mask)
+
+
+def augment_dataset(input_dir, output_dir, suffix="", seed=None):
+    if seed:
+        np.random.seed(seed)
+
+    images_paths, masks_paths = list_files(input_dir, validate_masks=True)
+    transformations = get_transformations()
+
+    process = ImageAugmentation(transformations, output_dir, suffix)
+    pool = multiprocessing.Pool()
+    pool.starmap(process, tqdm(zip(images_paths, masks_paths), total=len(images_paths)), chunksize=1)
 
 
 def main():
