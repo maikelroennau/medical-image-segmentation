186a98483e95f8496e8e2b6349bfb80ee23d63bc
Maikel Maciel Rönnau <maikel.ronnau@icloud.com>
2021-08-22 20:06:02 -0300

    Add new loss functions

--- utils.py
+++ utils.py
@@ -21,7 +21,8 @@ CUSTOM_OBJECTS = {
     "weighted_categorical_crossentropy": losses.weighted_categorical_crossentropy,
     "categorical_focal_loss": losses.categorical_focal_loss,
     "unified_focal_loss": losses.unified_focal_loss,
-    "categorical_crossentropy_plus_dice_loss": sm.metrics.iou_score,
+    "iou_score": sm.metrics.iou_score,
+    "categorical_crossentropy_plus_dice_loss": sm.losses.cce_dice_loss,
     "focal_loss_plus_dice_loss": sm.losses.categorical_focal_dice_loss
 }
 
