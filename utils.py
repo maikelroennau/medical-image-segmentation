d3f744136f77edd1930586ca8e7359bbae2d9f97
Maikel Maciel Rönnau <maikel.ronnau@icloud.com>
2021-08-22 21:48:30 -0300

    Add support for new decoders

--- utils.py
+++ utils.py
@@ -29,7 +29,7 @@ CUSTOM_OBJECTS = {
 METRICS = [
     "accuracy",
     losses.dice_coef,
-    losses.jaccard_index,
+    sm.metrics.iou_score
 ]
 
 def write_dataset(dataset, output_path="dataset_visualization", max_batches=None, same_dir=False):
@@ -165,7 +165,7 @@ def evaluate(model, images_path, batch_size, input_shape=None, classes=1, one_ho
         if input_shape:
             loaded_model = update_model(loaded_model, input_shape)
 
-        loaded_model.compile(optimizer=Adam(learning_rate=1e-5), loss=tf.keras.losses.CategoricalCrossentropy(), metrics=[METRICS])
+        loaded_model.compile(optimizer=Adam(learning_rate=1e-5), loss=sm.losses.cce_dice_loss, metrics=[METRICS])
 
         input_shape = loaded_model.input_shape[1:]
         height, width, channels = input_shape
@@ -213,7 +213,7 @@ def evaluate(model, images_path, batch_size, input_shape=None, classes=1, one_ho
             if input_shape:
                 loaded_model = update_model(loaded_model, input_shape)
 
-            loaded_model.compile(optimizer=Adam(learning_rate=1e-5), loss=tf.keras.losses.CategoricalCrossentropy(), metrics=[METRICS])
+            loaded_model.compile(optimizer=Adam(learning_rate=1e-5), loss=sm.losses.cce_dice_loss, metrics=[METRICS])
             evaluation_metrics = loaded_model.evaluate(evaluate_dataset)
             print(f"Model {str(model_path)}")
             print(f"  - Loss: {np.round(evaluation_metrics[0], 4)}")
